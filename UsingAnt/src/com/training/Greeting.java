8f8d01045925c515f38996057dd2e91f38b6d9c8
test <test@test-PC>
2015-08-05 16:39:52 +0530

    Added Overloaded Method

---
+++ UsingAnt/src/com/training/Greeting.java
@@ -0,0 +1,16 @@
+package com.training;
+
+public class Greeting {
+	
+	public String getMessage(String name)
+	{
+		return name + "Welcome";
+	}
+	
+	public String getMessage(){
+		System.out.println("Added Newly ");
+		
+		return "Welcome Ajay";
+	}
+
+}
